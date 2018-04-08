08f1a4f6b84d2b3c31ec102c45f9e2f66a75f95b
Kelly <hurtstotouchfire@gmail.com>
2018-04-07 22:51:36 -0400

    pinterest: Sleep before deleting TOS nag.

--- pinterest/nags.user.js
+++ pinterest/nags.user.js
@@ -3,4 +3,14 @@
 // @namespace Pinterest
 // @match https://www.pinterest.com/*
 // ==/UserScript==
-$('.Nags').remove()
+
+// sleep time expects milliseconds
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+// Wait for Pinterest to inject nags banner and then delete it
+sleep(2000).then(() => {
+  var x = document.querySelector('.Nags');
+  x.remove();
+});
