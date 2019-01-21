acd548bd9a3dc0f78386e4f29b61c031d8524a98
Kelly <hurtstotouchfire@gmail.com>
2019-01-21 18:56:06 -0500

    shoptagr: Sketched in price total function.

--- Shoptagr/listTotal.user.js
+++ Shoptagr/listTotal.user.js
@@ -11,12 +11,17 @@
     // TODO vet this boilerplate. Pseudocode follows.
     
     // Define page elements:
-    // listDescription = the tagline at the top, i.e. "12 products from 3 retailers"
-    // listItems = set of price tags
+    // the tagline at the top, i.e. "12 products from 3 retailers"
+    let listDescription = $('.topbar h3')
+    let listItems = $('.discount-price')
     
     // Loop through listItems and pull out price text, sanitize, convert to numbers
+    let prices = listItems.map(function(i) {
+        return $(listItems[i]).text().split("$")[1].trim();
+    })
+
     // Add up the numbers and append to listDescription, i.e. "12 products from 3 retailers, $45.71"
-    
+    console.log(prices);
 })();
 
 notificationnotification // wut?
