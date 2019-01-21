107429a999ed6c8a397204d6d3b1ee59a5b91f44
Kelly <hurtstotouchfire@gmail.com>
2019-01-20 23:10:52 -0500

    shoptagr: Add template for list total script.

---
+++ Shoptagr/listTotal.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Shoptagr List Total
+// @version      0.1
+// @description  Total items shown on the page in a Shoptagr wishlist
+// @author       https://github.com/hurtstotouchfire
+// @match        https://*.shoptagr.com/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // TODO vet this boilerplate. Pseudocode follows.
+    
+    // Define page elements:
+    // listDescription = the tagline at the top, i.e. "12 products from 3 retailers"
+    // listItems = set of price tags
+    
+    // Loop through listItems and pull out price text, sanitize, convert to numbers
+    // Add up the numbers and append to listDescription, i.e. "12 products from 3 retailers, $45.71"
+    
+})();
+
+notificationnotification // wut?
+
+close
